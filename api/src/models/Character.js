ac3c20be29939ffadc1068573f721d049d55e55b
Yuzalet <yuzaletg@gmail.com>
2022-05-18 00:02:17 -0500

    first commit - modelos y relaciones

--- api/src/models/Character.js
+++ api/src/models/Character.js
@@ -3,7 +3,41 @@ const { DataTypes } = require("sequelize");
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define("character", {
-
-  });
+  sequelize.define(
+    "character",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      species: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      origin: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      created: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
+      },
+    },
+    {
+      timestamps: true,
+      createdAt: "creado",
+      updatedAt: false,
+    }
+  );
 };
